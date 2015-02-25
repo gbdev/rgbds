261503c7c82e89807e678087d0125e1cd61bd6fb
Anthony J. Bentley <anthony@cathet.us>
2015-02-25 02:13:23 -0700

    Fix the reallocarray header by providing a prototype.

--- include/extern/reallocarray.h
+++ include/extern/reallocarray.h
@@ -1,10 +1,14 @@
 #ifndef EXTERN_REALLOCARRAY_H
 #define EXTERN_REALLOCARRAY_H
 
+#ifdef REALLOCARRAY_IN_LIBC
 #include <stdlib.h>
+#else
 
-#ifndef REALLOCARRAY_IN_LIBC
 #define reallocarray rgbds_reallocarray
+
+void *reallocarray(void *, size_t, size_t);
+
 #endif
 
 #endif
