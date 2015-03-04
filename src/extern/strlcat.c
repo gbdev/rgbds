eadaa4777082787e0718c54048bac6fc94301303
Anthony J. Bentley <anthony@cathet.us>
2015-03-03 23:15:05 -0700

    Sync with upstream.

--- src/extern/strlcat.c
+++ src/extern/strlcat.c
@@ -24,7 +24,7 @@
  * full size of dst, not space left).  At most dsize-1 characters
  * will be copied.  Always NUL terminates (unless dsize <= strlen(dst)).
  * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
- * If retval >= siz, truncation occurred.
+ * If retval >= dsize, truncation occurred.
  */
 size_t
 rgbds_strlcat(char *dst, const char *src, size_t dsize)
