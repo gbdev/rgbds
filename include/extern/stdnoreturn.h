e19507679341fc234a61bcdfb3685e61853543d2
stag019 <stag019@gmail.com>
2015-03-07 15:50:09 -0500

    Only block comments in headers

--- include/extern/stdnoreturn.h
+++ include/extern/stdnoreturn.h
@@ -1,16 +1,16 @@
 #if __STDC_VERSION__ >= 201112L
-    // C11 or newer
+    /* C11 or newer */
     #define noreturn _Noreturn
 #elif __cplusplus >= 201103L
-    // C++11 or newer
+    /* C++11 or newer */
     #define noreturn [[noreturn]]
 #elif __GNUC__ > 2 || (__GNUC__ == 2 && (__GNUC_MINOR__ >= 5))
-    // GCC 2.5 or newer
+    /* GCC 2.5 or newer */
     #define noreturn __attribute__ ((noreturn))
 #elif _MSC_VER >= 1310
-    // MS Visual Studio 2003/.NET Framework 1.1 or newer
+    /* MS Visual Studio 2003/.NET Framework 1.1 or newer */
     #define noreturn _declspec( noreturn)
 #else
-    // unsupported, but no need to throw a fit
+    /* unsupported, but no need to throw a fit */
     #define noreturn 
 #endif
