f431b384a2b8c1d90e5de30a270b73e1776e8cec
AntonioND <antonio_nd@outlook.com>
2017-04-03 20:46:18 +0100

    Add tests for rgblink options -t and -w
    Signed-off-by: AntonioND <antonio_nd@outlook.com>

--- test/link/update-refs.sh
+++ test/link/update-refs.sh
@@ -7,3 +7,13 @@ RGBLINK=../../rgblink
 $RGBASM -o $otemp bank-numbers.asm
 $RGBLINK -o $gbtemp $otemp > bank-numbers.out 2>&1
 head -c 20 $gbtemp > bank-numbers.out.bin 2>&1
+
+$RGBASM -o $otemp wramx-contwram.asm
+$RGBLINK -o $gbtemp $otemp > wramx-contwram-no-w.out 2>&1
+$RGBLINK -w -o $gbtemp $otemp > wramx-contwram-w.out 2>&1
+
+$RGBASM -o $otemp romx-tiny.asm
+$RGBLINK -o $gbtemp $otemp > romx-tiny-no-t.out 2>&1
+$RGBLINK -t -o $gbtemp $otemp > romx-tiny-t.out 2>&1
+
+exit 0
