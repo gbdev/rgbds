f8f67fcbceea754bba2268be11da9b7aba7a285a
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-01-27 11:14:10 +0000

    Add external projects to Travis CI jobs
    Small tests like the ones included in this repository are good to test
    individual features, but it is also a good idea to test some real
    projects.
    
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

---
+++ test/run-tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Return failure as soon as a command fails to execute
+
+set -e
+
+cd test
+
+# Tests included with the repository
+
+pushd asm
+./test.sh
+popd
+
+pushd link
+./test.sh
+popd
+
+# Test some significant external projects that use RGBDS
+
+git clone https://github.com/pret/pokecrystal.git --depth=1
+pushd pokecrystal
+make -j
+make compare
+popd
+
+git clone --recursive https://github.com/pret/pokered.git --depth=1
+pushd pokered
+make -j
+make compare
+popd
+
+git clone https://github.com/AntonioND/ucity.git --depth=1
+pushd ucity
+make -j
+popd
