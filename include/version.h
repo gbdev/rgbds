ea4276c7ac4537dd4daaec3286dd76a5bd4af921
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-03-20 20:11:32 +0000

    Increase version number to 0.3.6
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

--- include/version.h
+++ include/version.h
@@ -11,7 +11,7 @@
 
 #define PACKAGE_VERSION_MAJOR (0)
 #define PACKAGE_VERSION_MINOR (3)
-#define PACKAGE_VERSION_PATCH (5)
+#define PACKAGE_VERSION_PATCH (6)
 
 const char *get_package_version_string(void);
 
