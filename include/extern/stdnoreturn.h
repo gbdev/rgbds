29253046d59b7ae944f8d9c9164e27228852b4cf
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-04-02 00:19:33 +0100

    Remove C++ check in stdnoreturn.h
    This isn't a C++ project, only keep checks for C compilers.
    
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

--- include/extern/stdnoreturn.h
+++ include/extern/stdnoreturn.h
@@ -12,9 +12,6 @@
 #if __STDC_VERSION__ >= 201112L
 	/* C11 or newer */
 	#define noreturn _Noreturn
-#elif __cplusplus >= 201103L
-	/* C++11 or newer */
-	#define noreturn [[noreturn]]
 #elif __GNUC__ > 2 || (__GNUC__ == 2 && (__GNUC_MINOR__ >= 5))
 	/* GCC 2.5 or newer */
 	#define noreturn __attribute__ ((noreturn))
