e7dc094e568bf05688fd729e9a266ee02d2a2607
jidoc01 <jidoc01@naver.com>
2019-06-25 00:58:47 +0900

    Improve charmap structure with trie
    Charmap's previous structure was using brute-force comparison for
    converting the strings in source files. It always compared given
    strings to all of the strings in charmap, which was very costly
    in huge projects.
    For its improvement, I changed its structure into trie, which is
    being used in many string-processing areas. It's now much faster
    than before.

--- include/asm/charmap.h
+++ include/asm/charmap.h
@@ -13,14 +13,25 @@
 
 #define MAXCHARMAPS	512
 #define CHARMAPLENGTH	16
+#define MAXCHARNODES	(MAXCHARMAPS * CHARMAPLENGTH + 1)
+
+/*
+ * A node for trie structure.
+ */
+struct Charnode {
+	uint8_t code; /* the value in a key-value pair. */
+	uint8_t isCode; /* has one if it's a code node, not just a bridge node. */
+	struct Charnode *next[256]; /* each index representing the next possible character from its current state. */
+};
 
 struct Charmap {
-	int32_t count;
-	char input[MAXCHARMAPS][CHARMAPLENGTH + 1];
-	char output[MAXCHARMAPS];
+	int32_t charCount; /* user-side count. */
+	int32_t nodeCount; /* node-side count. */
+	struct Charnode nodes[MAXCHARNODES]; /* first node is reserved for the root node in charmap. */
 };
 
 int32_t readUTF8Char(char *destination, char *source);
+
 int32_t charmap_Add(char *input, uint8_t output);
 int32_t charmap_Convert(char **input);
 
