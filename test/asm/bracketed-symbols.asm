995265c5498cbabe86dd95026ffea03df42e02d7
ISSOtm <eldredhabert0@gmail.com>
2019-08-29 19:16:28 +0200

    Improve testing bracketed symbols Also test EQU and _RS constants, as well as that EQUS errors out, and that labels don't work.

--- test/asm/bracketed-symbols.asm
+++ test/asm/bracketed-symbols.asm
@@ -4,3 +4,17 @@ PRINTT "{x:X}\n"
 PRINTT "{X:X}\n"
 PRINTT "{d:X}\n"
 PRINTT "{b:X}\n"
+
+Y equ 1337
+PRINTT "{b:Y}\n"
+
+rsreset
+R rb 0
+PRINTT "{d:R}\n"
+
+S equs "You can't format me!"
+PRINTT "{X:S}\n"
+
+SECTION "Test", ROM0
+Label:
+PRINTT "{x:Label}\n"
