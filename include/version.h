323738e7b8426e59aa753ca1f1d623d5c1f24368
ISSOtm <eldredhabert0@gmail.com>
2019-11-01 17:38:43 +0100

    Increase version number to 0.3.9

--- include/version.h
+++ include/version.h
@@ -11,7 +11,7 @@
 
 #define PACKAGE_VERSION_MAJOR (0)
 #define PACKAGE_VERSION_MINOR (3)
-#define PACKAGE_VERSION_PATCH (8)
+#define PACKAGE_VERSION_PATCH (9)
 
 const char *get_package_version_string(void);
 
