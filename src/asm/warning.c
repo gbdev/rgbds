21f4cafef5a42e6368d1fcc5222adb6bea2d0b7e
ISSOtm <eldredhabert0@gmail.com>
2019-12-06 00:32:49 +0100

    Make `-Werror=` with a meta warning an error The previous behavior was to just enable the meta warning's warnings. This is an error now because it doesn't make sense to do that, does it?

--- src/asm/warning.c
+++ src/asm/warning.c
@@ -118,6 +118,9 @@ void processWarningFlag(char const *flag)
 		/* TODO: improve the matching performance? */
 		if (!strcmp(flag, warningFlags[id])) {
 			/* We got a match! */
+			if (setError)
+				errx(1, "Cannot make meta warning \"%s\" into an error", flag);
+
 			uint8_t const *ptr =
 					metaWarningCommands[id - NB_WARNINGS];
 
