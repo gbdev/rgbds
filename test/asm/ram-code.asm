ebda8255ffd6432b57a13fa79797aa0b0db339d2
ISSOtm <eldredhabert0@gmail.com>
2020-03-20 18:20:20 +0100

    Improve LOAD test to also test patches inside LOAD section

--- test/asm/ram-code.asm
+++ test/asm/ram-code.asm
@@ -1,13 +1,18 @@
 SECTION "test", ROM0[1]
 	call Target
 	LOAD "new", WRAM0[$C001]
-Target:	dl $DEADBEEF
+Target:	dl DEAD << 16 | BEEF
 .end
 	ENDL
 After:
 	jp Target
 	ld hl, Word
 	dw Byte, Target.end, After
+
+SECTION "dead", WRAMX[$DEAD]
+DEAD:
+SECTION "beef", SRAM[$BEEF]
+BEEF:
 
 SECTION "ram test", WRAM0 ; Should end up at $C005
 Word:
