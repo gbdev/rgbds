gbdev/rgbds

commit 5c24de3dc4e62c74fcd4f2a6a5055df73ee41a0a

Use inttypes for stdint types
This should help make RGBDS portable to systems with 16-bit integers,
like DOS.

For kicks, use the macros for 16-bit and 8-bit integers.

Fix other miscellaneous things, like #include ordering and other
printf-format related things.

Reduce repitition in math.c while I'm there.

include/linkdefs.h
@@ -13,8 +13,8 @@
 #include <stdint.h>
 
 #define RGBDS_OBJECT_VERSION_STRING "RGB%1hhu"
-#define RGBDS_OBJECT_VERSION_NUMBER (uint8_t)9
-#define RGBDS_OBJECT_REV 4
+#define RGBDS_OBJECT_VERSION_NUMBER 9
+#define RGBDS_OBJECT_REV 4U
 
 enum AssertionType {
 	ASSERT_WARN,

src/link/object.c
@@ -6,12 +6,13 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <errno.h>
+#include <inttypes.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <limits.h>
 
 #include "link/assign.h"
 #include "link/main.h"
@@ -212,25 +213,25 @@ static void readPatch(FILE *file, struct Patch *patch, char const *fileName,
 		      struct Section *fileSections[])
 {
 	tryReadstr(patch->fileName, file,
-		   "%s: Unable to read \"%s\"'s patch #%u's name: %s",
+		   "%s: Unable to read \"%s\"'s patch #%" PRIu32 "'s name: %s",
 		   fileName, sectName, i);
 	tryReadlong(patch->offset, file,
-		    "%s: Unable to read \"%s\"'s patch #%u's offset: %s",
+		    "%s: Unable to read \"%s\"'s patch #%" PRIu32 "'s offset: %s",
 		    fileName, sectName, i);
 	tryReadlong(patch->pcSectionID, file,
-		    "%s: Unable to read \"%s\"'s patch #%u's PC offset: %s",
+		    "%s: Unable to read \"%s\"'s patch #%" PRIu32 "'s PC offset: %s",
 		    fileName, sectName, i);
 	patch->pcSection = patch->pcSectionID == -1
 					? NULL
 					: fileSections[patch->pcSectionID];
 	tryReadlong(patch->pcOffset, file,
-		    "%s: Unable to read \"%s\"'s patch #%u's PC offset: %s",
+		    "%s: Unable to read \"%s\"'s patch #%" PRIu32 "'s PC offset: %s",
 		    fileName, sectName, i);
 	tryGetc(patch->type, file,
-		"%s: Unable to read \"%s\"'s patch #%u's type: %s",
+		"%s: Unable to read \"%s\"'s patch #%" PRIu32 "'s type: %s",
 		fileName, sectName, i);
 	tryReadlong(patch->rpnSize, file,
-		    "%s: Unable to read \"%s\"'s patch #%u's RPN size: %s",
+		    "%s: Unable to read \"%s\"'s patch #%" PRIu32 "'s RPN size: %s",
 		    fileName, sectName, i);
 
 	uint8_t *rpnExpression =
@@ -239,7 +240,7 @@ static void readPatch(FILE *file, struct Patch *patch, char const *fileName,
 				      patch->rpnSize, file);
 
 	if (nbElementsRead != patch->rpnSize)
-		errx(1, "%s: Cannot read \"%s\"'s patch #%u's RPN expression: %s",
+		errx(1, "%s: Cannot read \"%s\"'s patch #%" PRIu32 "'s RPN expression: %s",
 		     fileName, sectName, i,
 		     feof(file) ? "Unexpected end of file" : strerror(errno));
 	patch->rpnExpression = rpnExpression;
@@ -262,8 +263,8 @@ static void readSection(FILE *file, struct Section *section,
 	tryReadlong(tmp, file, "%s: Cannot read \"%s\"'s' size: %s",
 		    fileName, section->name);
 	if (tmp < 0 || tmp > UINT16_MAX)
-		errx(1, "\"%s\"'s section size (%d) is invalid", section->name,
-		     tmp);
+		errx(1, "\"%s\"'s section size (%" PRId32 ") is invalid",
+		     section->name, tmp);
 	section->size = tmp;
 	tryGetc(byte, file, "%s: Cannot read \"%s\"'s type: %s",
 		fileName, section->name);
@@ -273,7 +274,8 @@ static void readSection(FILE *file, struct Section *section,
 		    fileName, section->name);
 	section->isAddressFixed = tmp >= 0;
 	if (tmp > UINT16_MAX) {
-		error("\"%s\"'s org is too large (%d)", section->name, tmp);
+		error("\"%s\"'s org is too large (%" PRId32 ")",
+		      section->name, tmp);
 		tmp = UINT16_MAX;
 	}
 	section->org = tmp;
@@ -288,7 +290,7 @@ static void readSection(FILE *file, struct Section *section,
 	tryReadlong(tmp, file, "%s: Cannot read \"%s\"'s alignment offset: %s",
 		    fileName, section->name);
 	if (tmp > UINT16_MAX) {
-		error("\"%s\"'s alignment offset is too large (%d)",
+		error("\"%s\"'s alignment offset is too large (%" PRId32 ")",
 		      section->name, tmp);
 		tmp = UINT16_MAX;
 	}
@@ -371,7 +373,7 @@ static void readAssertion(FILE *file, struct Assertion *assert,
 {
 	char assertName[sizeof("Assertion #" EXPAND_AND_STR(UINT32_MAX))];
 
-	snprintf(assertName, sizeof(assertName), "Assertion #%u", i);
+	snprintf(assertName, sizeof(assertName), "Assertion #%" PRIu32, i);
 
 	readPatch(file, &assert->patch, fileName, assertName, 0, fileSections);
 	tryReadstr(assert->message, file, "%s: Cannot read assertion's message: %s",
@@ -417,7 +419,7 @@ void obj_ReadFile(char const *fileName)
 	tryReadlong(revNum, file, "%s: Cannot read revision number: %s",
 		    fileName);
 	if (revNum != RGBDS_OBJECT_REV)
-		errx(1, "%s is a revision 0x%04x object file, only 0x%04x is supported",
+		errx(1, "%s is a revision 0x%04" PRIx32 " object file; only 0x%04x is supported",
 		     fileName, revNum, RGBDS_OBJECT_REV);
 
 	uint32_t nbSymbols;
@@ -450,7 +452,7 @@ void obj_ReadFile(char const *fileName)
 
 	memset(nbSymPerSect, 0, sizeof(nbSymPerSect));
 
-	verbosePrint("Reading %u symbols...\n", nbSymbols);
+	verbosePrint("Reading %" PRIu32 " symbols...\n", nbSymbols);
 	for (uint32_t i = 0; i < nbSymbols; i++) {
 		/* Read symbol */
 		struct Symbol *symbol = malloc(sizeof(*symbol));
@@ -469,7 +471,7 @@ void obj_ReadFile(char const *fileName)
 	/* This file's sections, stored in a table to link symbols to them */
 	struct Section *fileSections[nbSections ? nbSections : 1];
 
-	verbosePrint("Reading %u sections...\n", nbSections);
+	verbosePrint("Reading %" PRIu32 " sections...\n", nbSections);
 	for (uint32_t i = 0; i < nbSections; i++) {
 		/* Read section */
 		fileSections[i] = malloc(sizeof(*fileSections[i]));
@@ -511,7 +513,7 @@ void obj_ReadFile(char const *fileName)
 
 	tryReadlong(nbAsserts, file, "%s: Cannot read number of assertions: %s",
 		    fileName);
-	verbosePrint("Reading %u assertions...\n", nbAsserts);
+	verbosePrint("Reading %" PRIu32 " assertions...\n", nbAsserts);
 	for (uint32_t i = 0; i < nbAsserts; i++) {
 		struct Assertion *assertion = malloc(sizeof(*assertion));
 

src/asm/asmy.y
@@ -9,6 +9,7 @@
 %{
 #include <ctype.h>
 #include <errno.h>
+#include <inttypes.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -79,7 +80,7 @@ size_t symvaluetostring(char *dest, size_t maxLength, char *symName,
 			strncpy(dest, write_ptr, maxLength + 1);
 		} else {
 			fullLength = snprintf(dest, maxLength + 1,
-							  mode ? mode : "$%X",
+							  mode ? mode : "$%" PRIX32,
 						      value);
 		}
 

src/asm/fstack.c
@@ -10,14 +10,15 @@
  * FileStack routines
  */
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <errno.h>
+#include <inttypes.h>
 #include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #include "asm/fstack.h"
@@ -69,7 +70,7 @@ static void pushcontext(void)
 	struct sContext **ppFileStack;
 
 	if (++nFileStackDepth > nMaxRecursionDepth)
-		fatalerror("Recursion limit (%d) exceeded", nMaxRecursionDepth);
+		fatalerror("Recursion limit (%u) exceeded", nMaxRecursionDepth);
 
 	ppFileStack = &pFileStack;
 	while (*ppFileStack)
@@ -271,12 +272,12 @@ void fstk_Dump(void)
 	pLastFile = pFileStack;
 
 	while (pLastFile) {
-		fprintf(stderr, "%s(%d) -> ", pLastFile->tzFileName,
+		fprintf(stderr, "%s(%" PRId32 ") -> ", pLastFile->tzFileName,
 			pLastFile->nLine);
 		pLastFile = pLastFile->pNext;
 	}
 
-	fprintf(stderr, "%s(%d)", tzCurrentFileName, nLineNo);
+	fprintf(stderr, "%s(%" PRId32 ")", tzCurrentFileName, nLineNo);
 }
 
 void fstk_DumpToStr(char *buf, size_t buflen)
@@ -286,7 +287,7 @@ void fstk_DumpToStr(char *buf, size_t buflen)
 	size_t len = buflen;
 
 	while (pLastFile) {
-		retcode = snprintf(&buf[buflen - len], len, "%s(%d) -> ",
+		retcode = snprintf(&buf[buflen - len], len, "%s(%" PRId32 ") -> ",
 				   pLastFile->tzFileName, pLastFile->nLine);
 		if (retcode < 0)
 			fatalerror("Failed to dump file stack to string: %s",
@@ -298,8 +299,8 @@ void fstk_DumpToStr(char *buf, size_t buflen)
 		pLastFile = pLastFile->pNext;
 	}
 
-	retcode = snprintf(&buf[buflen - len], len, "%s(%d)", tzCurrentFileName,
-			   nLineNo);
+	retcode = snprintf(&buf[buflen - len], len, "%s(%" PRId32 ")",
+			   tzCurrentFileName, nLineNo);
 	if (retcode < 0)
 		fatalerror("Failed to dump file stack to string: %s",
 			   strerror(errno));

src/asm/lexer.c
@@ -7,12 +7,13 @@
  */
 
 #include <assert.h>
+#include <ctype.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
-#include <ctype.h>
 
 #include "asm/asm.h"
 #include "asm/fstack.h"
@@ -120,7 +121,7 @@ void yyunputstr(const char *s)
 void lex_BeginStringExpansion(const char *tzName)
 {
 	if (++nNbStringExpansions > nMaxRecursionDepth)
-		fatalerror("Recursion limit (%d) exceeded", nMaxRecursionDepth);
+		fatalerror("Recursion limit (%u) exceeded", nMaxRecursionDepth);
 
 	struct sStringExpansionPos *pNewStringExpansion =
 		malloc(sizeof(*pNewStringExpansion));
@@ -371,7 +372,7 @@ uint32_t lex_FloatAlloc(const struct sLexFloat *token)
 bool lex_CheckCharacterRange(uint16_t start, uint16_t end)
 {
 	if (start > end || start < 1 || end > 127) {
-		yyerror("Invalid character range (start: %u, end: %u)",
+		yyerror("Invalid character range (start: %" PRIu16 ", end: %" PRIu16 ")",
 			start, end);
 		return false;
 	}
@@ -672,7 +673,7 @@ size_t yylex_ReadBracketedSymbol(char *dest, size_t index)
 			 * so it's handled differently
 			 */
 			static const char * const formatSpecifiers[] = {
-				"", "%x", "%X", "%d"
+				"", "%" PRIx32, "%" PRIX32, "%" PRId32
 			};
 			/* Prevent reading out of bounds! */
 			const char *designatedMode;

src/asm/macro.c
@@ -1,5 +1,6 @@
 
 #include <assert.h>
+#include <inttypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -82,7 +83,7 @@ void macro_SetUniqueID(uint32_t id)
 		uniqueIDPtr = NULL;
 	} else {
 		/* The buffer is guaranteed to be the correct size */
-		sprintf(uniqueIDBuf, "_%u", id);
+		sprintf(uniqueIDBuf, "_%" PRIu32, id);
 		uniqueIDPtr = uniqueIDBuf;
 	}
 }

src/asm/main.c
@@ -167,7 +167,7 @@ void opt_Parse(char *s)
 		/* fallthrough */
 	case 'p':
 		if (strlen(&s[1]) <= 2) {
-			int32_t result;
+			int result;
 			unsigned int fillchar;
 
 			result = sscanf(&s[1], "%x", &fillchar);
@@ -551,10 +551,11 @@ int main(int argc, char *argv[])
 		fclose(dependfile);
 
 	if (nIFDepth != 0)
-		errx(1, "Unterminated IF construct (%u levels)!", nIFDepth);
+		errx(1, "Unterminated IF construct (%" PRIu32 " levels)!",
+		     nIFDepth);
 
 	if (nUnionDepth != 0) {
-		errx(1, "Unterminated UNION construct (%u levels)!",
+		errx(1, "Unterminated UNION construct (%" PRIu32 " levels)!",
 		     nUnionDepth);
 	}
 
@@ -564,8 +565,9 @@ int main(int argc, char *argv[])
 	timespent = ((double)(nEndClock - nStartClock))
 		     / (double)CLOCKS_PER_SEC;
 	if (verbose) {
-		printf("Success! %u lines in %d.%02d seconds ", nTotalLines,
-		       (int)timespent, ((int)(timespent * 100.0)) % 100);
+		printf("Success! %" PRIu32 " lines in %d.%02d seconds ",
+		       nTotalLines, (int)timespent,
+		       ((int)(timespent * 100.0)) % 100);
 		if (timespent < FLT_MIN_EXP)
 			printf("(INFINITY lines/minute)\n");
 		else

src/asm/math.c
@@ -10,6 +10,7 @@
  * Fixedpoint math routines
  */
 
+#include <inttypes.h>
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -37,12 +38,16 @@ void math_DefinePI(void)
  */
 void math_Print(int32_t i)
 {
-	if (i >= 0)
-		printf("%d.%05d", i >> 16,
-		       ((int32_t)(fx2double(i) * 100000 + 0.5)) % 100000);
-	else
-		printf("-%d.%05d", (-i) >> 16,
-		       ((int32_t)(fx2double(-i) * 100000 + 0.5)) % 100000);
+	uint32_t u = i;
+	const char *sign = "";
+
+	if (i < 0) {
+		u = -u;
+		sign = "-";
+	}
+
+	printf("%s%" PRIu32 ".%05" PRIu32, sign, u >> 16,
+	       ((uint32_t)(fx2double(u) * 100000 + 0.5)) % 100000);
 }
 
 /*

commit 240fca0861d6924403b39ba8c2752a67137fb45a

Fix Clang warning in linkdefs
This one's really stupid; printing an int with %hhu gives a warning
even though a regular unsigned char is promoted to an int before
printing anyway. Silence it so the build with Clang works.

This is a regression introduced in 5c24de3

include/linkdefs.h
@@ -12,8 +12,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define RGBDS_OBJECT_VERSION_STRING "RGB%1hhu"
-#define RGBDS_OBJECT_VERSION_NUMBER 9
+#define RGBDS_OBJECT_VERSION_STRING "RGB%1u"
+#define RGBDS_OBJECT_VERSION_NUMBER 9U
 #define RGBDS_OBJECT_REV 4U
 
 enum AssertionType {

src/link/object.c
@@ -400,18 +400,18 @@ void obj_ReadFile(char const *fileName)
 		err(1, "Could not open file %s", fileName);
 
 	/* Begin by reading the magic bytes and version number */
-	uint8_t versionNumber;
+	unsigned versionNumber;
 	int matchedElems = fscanf(file, RGBDS_OBJECT_VERSION_STRING,
 				  &versionNumber);
 
 	if (matchedElems != 1)
 		errx(1, "\"%s\" is not a RGBDS object file", fileName);
 
-	verbosePrint("Reading object file %s, version %hhu\n",
+	verbosePrint("Reading object file %s, version %u\n",
 		     fileName, versionNumber);
 
 	if (versionNumber != RGBDS_OBJECT_VERSION_NUMBER)
-		errx(1, "\"%s\" is an incompatible version %hhu object file",
+		errx(1, "\"%s\" is an incompatible version %u object file",
 		     fileName, versionNumber);
 
 	uint32_t revNum;