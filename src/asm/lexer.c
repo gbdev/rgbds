dbef51ba052ff9d98fbed87f36b53e5a2d4af074
ISSOtm <eldredhabert0@gmail.com>
2020-08-23 02:09:40 +0200

    Move `isWhitespace` to a place where it makes more sense

--- src/asm/lexer.c
+++ src/asm/lexer.c
@@ -232,6 +232,11 @@ static struct KeywordMapping {
 	{"OPT", T_POP_OPT}
 };
 
+static bool isWhitespace(int c)
+{
+	return c == ' ' || c == '\t';
+}
+
 #define LEXER_BUF_SIZE 42 /* TODO: determine a sane value for this */
 /* This caps the size of buffer reads, and according to POSIX, passing more than SSIZE_MAX is UB */
 static_assert(LEXER_BUF_SIZE <= SSIZE_MAX, "Lexer buffer size is too large");
@@ -892,11 +897,6 @@ static void discardComment(void)
 }
 
 /* Function to read a line continuation */
-
-static bool isWhitespace(int c)
-{
-	return c == ' ' || c == '\t';
-}
 
 static void readLineContinuation(void)
 {
