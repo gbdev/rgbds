855613670e0dc6e480b25ca976b8998d32f67a4e
Rangi <remy.oukaour+rangi42@gmail.com>
2020-12-12 18:23:26 -0500

    Implement `\#` to expand to all unshifted macro arguments
    Fixes #596

---
+++ test/asm/macro-#.asm
@@ -0,0 +1,46 @@
+SECTION "Test", ROM0
+
+list: MACRO
+	db _NARG
+if _NARG > 0
+	db \#
+endc
+	db -1
+ENDM
+
+	list
+	list 42
+	list $aa, $bb, $cc, $dd, $ee
+
+person: MACRO
+	db \1, \2, \3, \4, \5
+ENDM
+
+object: MACRO
+x = \1
+y = \2
+	shift 2
+	person y, x, \#
+ENDM
+
+	person  5, 10, $33, $44, $55
+	object 12,  6, $66, $77, $88
+
+echo: MACRO
+	printt "\#\n"
+ENDM
+
+R EQUS "S"
+
+	echo P
+	echo Q,R, {R},  T
+	echo 42,$2a
+
+print: MACRO
+	printt STRCAT(\#)
+	printt "\n"
+ENDM
+
+	print
+	print "A"
+	print "B", "C",  "D"
