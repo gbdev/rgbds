7bb6f71f0b51afd06446fe5138f5e90f996af7e4
Rangi <35663410+Rangi42@users.noreply.github.com>
2021-01-01 18:46:26 -0500

    Change `FOREACH` to `FOR` (#680)

--- test/asm/for.asm
+++ test/asm/for.asm
@@ -1,44 +1,44 @@
-foreach n, 10
+for n, 10
 	printt "{d:n} "
 endr
 	printt "-> {d:n}\n"
 
-foreach v, 0
+for v, 0
 	printt "unreached"
 endr
 
-foreach v, 2, 1
+for v, 2, 1
 	printt "unreached"
 endr
 
-foreach v, 1, 2, 0
+for v, 1, 2, 0
 	printt "unreached"
 endr
 
-foreach x, 1, 5+1
+for x, 1, 5+1
 	printt "{d:x} "
 endr
 	printt "-> {d:x}\n"
 
-foreach v, 10, -1, -1
+for v, 10, -1, -1
 	printt "{d:v} "
 v = 42
 endr
 	printt "-> {d:v}\n"
 
-foreach q, 5, 21, 5
+for q, 5, 21, 5
 	printt "{d:q} "
 purge q
 endr
 	printt "-> {d:q}\n"
 
 s EQUS "x"
-foreach s, 3, 30, 3
+for s, 3, 30, 3
 	printt "{d:x} "
 endr
 	printt "-> {d:x}\n"
 
-foreach v, 10
+for v, 10
 	printt "{d:v}\n"
 if v == 3
 purge v
