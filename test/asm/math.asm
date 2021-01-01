b15b5ee23d99903089f6648bdbb67715e7f4de16
Rangi <remy.oukaour+rangi42@gmail.com>
2021-01-01 13:03:12 -0500

    Update test/asm/math.asm to cover RGBASM and RGBLINK
    Expressions are implemented separately for both

--- test/asm/math.asm
+++ test/asm/math.asm
@@ -1,5 +1,18 @@
-	assert 2*10**2*2 == 400
-	assert -3**4 == -81
+X equ 0
+
+test: MACRO
+; Test RGBASM
+v equs "X +"
+	static_assert \#
+	purge v
+; Test RGBLINK
+v equs "Y +"
+	assert \#
+	purge v
+ENDM
+
+	test (v 2)*(v 10)**(v 2)*(v 2) == (v 400)
+	test -(v 3)**(v 4) == v -81
 
 	assert DIV(5.0, 2.0) == 2.5
 	assert DIV(-5.0, 2.0) == -2.5
@@ -22,3 +35,6 @@
 
 	assert FLOOR(1.5) == 1.0
 	assert FLOOR(-1.5) == -2.0
+
+SECTION "Y", ROM0
+Y::
