7ce5cf1595f2fb3a508e920d8e4929381e298e59
Rangi <remy.oukaour+rangi42@gmail.com>
2021-01-04 10:49:30 -0500

    Convert floating to fixed point by rounding, not truncation
    Fixes #678

---
+++ test/asm/fixed-point-precision.asm
@@ -0,0 +1,11 @@
+f1 = 3.1
+f2 = 5.2
+pm = MUL(f1, f2)
+pr = 16.12
+	println "`3.1`:   {9.6f:f1} -> ${08x:f1}"
+	println "`5.2`:   {9.6f:f2} -> ${08x:f2}"
+	println "`MUL`:   {9.6f:pm} -> ${08x:pm}"
+	println "`16.12`: {9.6f:pr} -> ${08x:pr}"
+
+fl = 6.283185
+	println "`6.283185`: {.6f:fl} -> ${08x:fl}"
