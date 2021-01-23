6623b1dc458633d344c83a4ba8f0310f9c6a3090
ISSOtm <eldredhabert0@gmail.com>
2021-01-23 01:50:17 +0100

    Fix CI on macOS
    Apple supplies version 2.3 (from 2006!!), which doesn't support `%empty`.

--- .github/actions/install_deps.sh
+++ .github/actions/install_deps.sh
@@ -4,7 +4,9 @@ case `echo $1 | cut -d '-' -f 1` in
 		sudo apt-get install -yq bison libpng-dev pkg-config
 		;;
 	macos)
-		brew install libpng pkg-config md5sha1sum
+		brew install bison libpng pkg-config md5sha1sum
+		# For the version check below exclusively, re-do this before building
+		export PATH="/usr/local/opt/bison/bin:$PATH"
 		;;
 	*)
 		echo "WARNING: Cannot install deps for OS '$1'"
