a09f2d4115cd65b4bbe242421bccabf2f8dba25e
Rangi <remy.oukaour+rangi42@gmail.com>
2021-02-23 14:48:27 -0500

    Test an indented macro label

--- test/asm/label-indent.asm
+++ test/asm/label-indent.asm
@@ -5,26 +5,7 @@ SECTION "Label testing", WRAMX
 	.loc
 	Lab.loc2
 
-;	X = 0
-;	; Should not believe X is a label!
-;	IF X == 1
-;		FAIL "Wrong!"
-;	ENDC
-;	X \
-;	  = 1
-;	IF X != 1
-;		FAIL "Wrong!"
-;	ENDC
-;
-;	Y equ 42
-;	PRINTT "Y={Y}\n"
-;	PURGE Y
-;
-;	Y equs "mac"
-;	mac: MACRO
-;		PRINTT "\1\n"
-;	ENDM
-;	; Should invoke macro Y, and not match "equ" due to the "a" behind it
-;	Y equates 69
-;	Y equsquisite (isn't that English?)
-;	Y eq
+	mac: MACRO
+		println "\1"
+	ENDM
+	mac :
