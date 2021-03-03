b674c5fcaa93b32fdffe419672c222596cdf74d9
Rangi <remy.oukaour+rangi42@gmail.com>
2021-03-03 10:35:47 -0500

    Comment refers to "built-in" symbols

--- src/asm/output.c
+++ src/asm/output.c
@@ -473,7 +473,7 @@ static void registerUnregisteredSymbol(struct Symbol *symbol, void *arg)
 {
 	(void)arg; // sym_ForEach requires a void* parameter, but we are not using it.
 
-	// Check for symbol->src, to skip any auto generated symbol from rgbasm
+	// Check for symbol->src, to skip any built-in symbol from rgbasm
 	if (symbol->src && symbol->ID == -1) {
 		registerSymbol(symbol);
 	}
