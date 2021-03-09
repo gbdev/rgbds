3ce3d2f6626e95ccda872a8f30b27333841ad628
ISSOtm <eldredhabert0@gmail.com>
2021-03-09 22:56:59 +0100

    Explicitly force tested projects to use tested bins

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -23,8 +23,6 @@ done
 # Test some significant external projects that use RGBDS
 # When adding new ones, don't forget to add them to the .gitignore!
 
-export PATH="$PWD/..:$PATH"
-
 if [ ! -d pokecrystal ]; then
 	git clone https://github.com/pret/pokecrystal.git --shallow-since=2018-06-04 --single-branch
 fi
@@ -32,7 +30,7 @@ pushd pokecrystal
 git fetch
 git checkout 5db892782adaa5bb5a3e4cd6fa06565764bd1fb0
 make clean
-make -j4 compare
+make -j4 compare RGBDS=../../
 popd
 
 if [ ! -d pokered ]; then
@@ -42,7 +40,7 @@ pushd pokered
 git fetch
 git checkout 21908ba30a8bae5c5e1c86b1164402ec95da0220
 make clean
-make -j4 compare
+make -j4 compare RGBDS=../../
 popd
 
 if [ ! -d ucity ]; then
@@ -52,5 +50,5 @@ pushd ucity
 git fetch
 git checkout 15be184b26b337110e1ec2998cd42f134f00f281
 make clean
-make -j4
+make -j4 RGBDS=../../
 popd
