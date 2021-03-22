90465993a2632085593c4675eb5a5c10d732aaff
Rangi <remy.oukaour+rangi42@gmail.com>
2021-03-22 00:25:22 -0400

    Do not evaluate an untaken ELIF's condition
    Fixes #764

--- test/asm/skip-elif-condition.asm
+++ test/asm/skip-elif-condition.asm
@@ -3,7 +3,7 @@ mac: MACRO
 		println "small \1"
 	elif (\1) > 100
 		println "large \1"
-	elif (\1) / 0 == 42 ; only evaluated if the "large" condition was taken
+	elif (\1) / 0 == 42 ; only evaluated if neither "small" nor "large" was taken
 		println "division by zero!?"
 	elif syntax! error?
 		println "X_X"
@@ -15,3 +15,4 @@ ENDM
 	mac 2 + 2
 	mac STRLEN("abcdef")
 	mac 101
+	mac 23
