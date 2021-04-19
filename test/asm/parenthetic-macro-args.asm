637bbbdf4312ca6efb6eb4ad6ae4ad7f06d9357e
Rangi <remy.oukaour+rangi42@gmail.com>
2021-04-18 23:26:03 -0400

    Support multi-digit macro arguments in parentheses
    This allows access to arguments past \9 without using 'shift'

---
+++ test/asm/parenthetic-macro-args.asm
@@ -0,0 +1,17 @@
+MACRO printargs
+	PRINTLN "first = \(1)"
+	FOR I, 2, _NARG
+		PRINTLN "next = \({d:I})"
+	ENDR
+	PRINTLN "last = \({d:_NARG})"
+ENDM
+
+	printargs A, B, C, D
+
+MACRO mac
+	println \(2__) + \(1_2) + \(\1)
+x = 2
+	println \({d:x}) + \(1_{d:x}) + \(\(\(13)))
+ENDM
+
+	mac 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 1
