e9bfe849adfee7d9b7ef2dfef9f1896bda8a5a9b
Rangi <remy.oukaour+rangi42@gmail.com>
2021-05-01 23:04:57 -0400

    Allow `OPT` to toggle `-h`

--- test/asm/opt.asm
+++ test/asm/opt.asm
@@ -1,14 +1,16 @@
 SECTION "test", ROM0
 
-	opt !L ; already the default, but tests parsing "!L"
+	opt !h, !L ; already the default, but tests parsing "!"
 
 pusho
-	opt p42, L, Wno-div
+	opt p42, h, L, Wno-div
 	ds 1
 	ld [$ff88], a
+	halt
 	println $8000_0000 / -1
 popo
 
 	ds 1
 	ld [$ff88], a
+	halt
 	println $8000_0000 / -1
