f97340110f210b7b5043873c769e95ee72e305c9
rlewicki <contact@rlewicki.tech>
2022-10-24 00:26:28 +0200

    #1082 Add cache check for external repositories used during testing

---
+++ test/fetch-test-deps.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+echo "Fetching test dependency repositories"
+
+test_downstream() { # owner/repo shallow-since commit make-target
+	if [ ! -d ${1##*/} ]; then
+		git clone https://github.com/$1.git --shallow-since=$2 --single-branch
+	fi
+	pushd ${1##*/}
+	git checkout -f $3
+	if [ -f ../patches/${1##*/}.patch ]; then
+		git apply --ignore-whitespace ../patches/${1##*/}.patch
+	fi
+	popd
+}
+
+test_downstream pret/pokecrystal 2022-09-29 70a3ec1accb6de1c1c273470af0ddfa2edc1b0a9
+test_downstream pret/pokered     2022-09-29 2b52ceb718b55dce038db24d177715ae4281d065
+test_downstream AntonioND/ucity  2022-04-20 d8878233da7a6569f09f87b144cb5bf140146a0f
--- test/run-tests.sh
+++ test/run-tests.sh
@@ -26,19 +26,12 @@ done
 # to the day before, to reduce the amount of refs being transferred and thus speed up CI.
 
 test_downstream() { # owner/repo shallow-since commit make-target
-	if [ ! -d ${1##*/} ]; then
-		git clone https://github.com/$1.git --shallow-since=$2 --single-branch
-	fi
 	pushd ${1##*/}
-	git checkout -f $3
-	if [ -f ../patches/${1##*/}.patch ]; then
-		git apply --ignore-whitespace ../patches/${1##*/}.patch
-	fi
 	make clean
-	make -j4 $4 RGBDS=../../
+	make -j4 $2 RGBDS=../../
 	popd
 }
 
-test_downstream pret/pokecrystal 2022-09-29 70a3ec1accb6de1c1c273470af0ddfa2edc1b0a9 compare
-test_downstream pret/pokered     2022-09-29 2b52ceb718b55dce038db24d177715ae4281d065 compare
-test_downstream AntonioND/ucity  2022-04-20 d8878233da7a6569f09f87b144cb5bf140146a0f ''
+test_downstream pret/pokecrystal compare
+test_downstream pret/pokered compare
+test_downstream AntonioND/ucity ''
