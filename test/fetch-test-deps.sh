2e91547cbaacbf025843beb3a6af44e61e706297
Damian Yerrick <pino@pineight.com>
2023-08-17 14:16:14 -0400

    add -h and --dfsg options to tests
    Use getopt(1) to provide -h, --help, and --dfsg options in
    test/fetch-test-deps.sh and test/run-tests.sh
    
    - -h or --help prints a usage message
    - --dfsg skips repositories with non-free licenses (pokecrystal
      and pokered) in favor of ucity to support packaging RGBDS for
      Debian
    - --dfsg can be combined with --get-hash or --get-paths
    
    change --dfsg option to --only-free
    
    I was told the `+dfsg.1` used in Debian's repacks of upstream repos
    and tarballs that contain non-free software is too obscure.  ISSOtm
    and Rangi42 suggested `--only-free` which carries no Debian baggage.
    https://www.reddit.com/r/debian/comments/66094l/what_is_dfsg_in_package_version_numbers/
    
    - change --dfsg option to --only-free
    - temporarily enable logging (set -x) to troubleshoot build failure
      on macOS
    
    don't depend on util-linux getopt
    
    util-linux getopt has long option parsing functionality not
    available in macOS getopt.  Instead, parse the options in pure
    Bash, treating each option as a separate word.
    
    remove tracing from test
    
    The removal of getopt indeed fixed test scripts on macOS.
    This means we no longer need set -x to trace execution.
    
    Co-authored-by: Eldred Habert <eldredhabert0@gmail.com>
    
    use true/false for booleans in test scripts
    
    I haven't had a chance to read the `bash(1)` manual cover to cover
    (reading 6600 lines in `less(1)` is an attention challenge to me)
    but it appears `true` and `false` are preferred because `true(1)`
    and `false(1)` run faster than `test(1)`, to which `[` is a link.
    
    Co-authored-by: Eldred Habert <eldredhabert0@gmail.com>
    
    act on stylistic nitpicks by Rangi42
    
    - remove explanation of `set -e` even from run-tests.sh, which had
      previously included one
    - visually distinguish explanations of sections of a script from
      explanations of individual functions within a section
    - change variable names that refer to script names to match the
      capitalized default filename of the script minus any extension
    - change variable names that do not refer to script names to
      lowercase
    
    Apply suggestions from code review
    
    Co-authored-by: Rangi <35663410+Rangi42@users.noreply.github.com>
    
    remane with_nonfree variable in tests to nonfree
    
    per @Rangi42's preference

--- test/fetch-test-deps.sh
+++ test/fetch-test-deps.sh
@@ -1,10 +1,45 @@
 #!/usr/bin/env bash
-
 set -e
 
 cd "$(dirname "$0")"
 
-case "$1" in
+usage() {
+	echo "Downloads source code of Game Boy programs used as RGBDS test cases."
+	echo "Options:"
+	echo "    -h, --help      show this help message"
+	echo "    --only-free     download only freely licensed codebases"
+	echo "    --get-hash      print programs' commit hashes instead of downloading them"
+	echo "    --get-paths     print programs' GitHub paths instead of downloading them"
+}
+
+# Parse options in pure Bash because macOS `getopt` is stuck
+# in what util-linux `getopt` calls `GETOPT_COMPATIBLE` mode
+nonfree=true
+actionname=
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		-h|--help)
+			usage
+			exit 0
+			;;
+		--only-free)
+			nonfree=false
+			;;
+		--get-hash|--get-paths)
+			actionname="$1"
+			;;
+		--)
+			break
+			;;
+		*)
+			echo "$(basename $0): unknown option "$1""
+			exit 1
+			;;
+	esac
+	shift
+done
+
+case "$actionname" in
 	--get-hash)
 		action() { # owner/repo shallow-since commit
 			printf "%s@%s-" "${1##*/}" "$3"
@@ -33,6 +68,8 @@ case "$1" in
 		}
 esac
 
-action pret/pokecrystal 2022-09-29 70a3ec1accb6de1c1c273470af0ddfa2edc1b0a9
-action pret/pokered     2022-09-29 2b52ceb718b55dce038db24d177715ae4281d065
-action AntonioND/ucity  2022-04-20 d8878233da7a6569f09f87b144cb5bf140146a0f
+if "$nonfree"; then
+	action pret/pokecrystal 2022-09-29 70a3ec1accb6de1c1c273470af0ddfa2edc1b0a9
+	action pret/pokered     2022-09-29 2b52ceb718b55dce038db24d177715ae4281d065
+fi
+action         AntonioND/ucity  2022-04-20 d8878233da7a6569f09f87b144cb5bf140146a0f
