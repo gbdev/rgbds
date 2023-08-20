7b3a05eea8a11f68ae62b8298b3d987d356fb4f4
Damian Yerrick <pino@pineight.com>
2023-08-20 13:46:00 -0400

    Add `--only-free` and `--help` options to test scripts (#1161)

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,10 +1,39 @@
 #!/usr/bin/env bash
-
-# Return failure as soon as a command fails to execute
-
 set -e
 
 cd "$(dirname "$0")"
+
+usage() {
+	echo "Runs regression tests on RGBDS."
+	echo "Options:"
+	echo "    -h, --help      show this help message"
+	echo "    --only-free     skip tests that build nonfree codebases"
+}
+
+# Parse options in pure Bash because macOS `getopt` is stuck
+# in what util-linux `getopt` calls `GETOPT_COMPATIBLE` mode
+nonfree=true
+FETCH_TEST_DEPS="fetch-test-deps.sh"
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		-h|--help)
+			usage
+			exit 0
+			;;
+		--only-free)
+			nonfree=false
+			FETCH_TEST_DEPS="fetch-test-deps.sh --only-free"
+			;;
+		--)
+			break
+			;;
+		*)
+			echo "$(basename $0): internal error"
+			exit 1
+			;;
+	esac
+	shift
+done
 
 # Refuse to run if RGBDS isn't present
 if [[ ! ( -x ../rgbasm && -x ../rgblink && -x ../rgbfix && -x ../rgbgfx ) ]]; then
@@ -27,7 +56,7 @@ done
 
 test_downstream() { # owner/repo make-target
 	if ! pushd ${1##*/}; then
-		echo >&2 'Please run `fetch-test-deps.sh` before running the test suite'
+		echo >&2 'Please run `'"$FETCH_TEST_DEPS"'` before running the test suite'
 		return 1
 	fi
 	make clean
@@ -35,6 +64,8 @@ test_downstream() { # owner/repo make-target
 	popd
 }
 
-test_downstream pret/pokecrystal compare
-test_downstream pret/pokered     compare
+if "$nonfree"; then
+	test_downstream pret/pokecrystal compare
+	test_downstream pret/pokered     compare
+fi
 test_downstream AntonioND/ucity  ''
