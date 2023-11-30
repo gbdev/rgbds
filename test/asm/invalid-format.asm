6a0fac78f3d1f9f0a192ca1c632ad9790ffed796
Rangi42 <remy.oukaour+rangi42@gmail.com>
2023-11-30 12:03:23 -0500

    Add more tests

---
+++ test/asm/invalid-format.asm
@@ -0,0 +1,14 @@
+println STRFMT("%+d %++d", 42, 42)
+println STRFMT("%#x %##x", 42, 42)
+println STRFMT("%-4d %--4d", 42, 42)
+
+DEF N = 42
+println "{5d:N} {5d5:N}"
+
+println STRFMT("%+s", "hello")
+println STRFMT("%#s", "hello")
+println STRFMT("%0s", "hello")
+println STRFMT("%.5s", "hello")
+
+println STRFMT("%#d", 42)
+println STRFMT("%.5d", 42)
