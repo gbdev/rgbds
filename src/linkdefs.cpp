831b8006fa55c32fcfcc0faeb1417f14f4928fb0
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-03-04 12:51:39 -0500

    Use different comment style to fix array formatting

--- src/linkdefs.cpp
+++ src/linkdefs.cpp
@@ -10,64 +10,56 @@ using namespace std::literals;
 // info, so RGBASM's job is only to catch unconditional errors earlier.
 SectionTypeInfo sectionTypeInfo[SECTTYPE_INVALID] = {
     {
-     // SECTTYPE_WRAM0
-        .name = "WRAM0"s,
+     .name = "WRAM0"s,
      .startAddr = 0xC000,
-     .size = 0x2000,                              // Patched to 0x1000 if !isWRAM0Mode
-        .firstBank = 0,
+     .size = 0x2000 /* Patched to 0x1000 if !isWRAM0Mode */,
+     .firstBank = 0,
      .lastBank = 0,
      },
     {
-     // SECTTYPE_VRAM
-        .name = "VRAM"s,
+     .name = "VRAM"s,
      .startAddr = 0x8000,
      .size = 0x2000,
      .firstBank = 0,
-     .lastBank = 1,                                      // Patched to 0 if isDmgMode
-    },
+     .lastBank = 1 /* Patched to 0 if isDmgMode */,
+     },
     {
-     // SECTTYPE_ROMX
-        .name = "ROMX"s,
+     .name = "ROMX"s,
      .startAddr = 0x4000,
      .size = 0x4000,
      .firstBank = 1,
      .lastBank = 65535,
      },
     {
-     // SECTTYPE_ROM0
-        .name = "ROM0"s,
+     .name = "ROM0"s,
      .startAddr = 0x0000,
-     .size = 0x8000,                              // Patched to 0x4000 if !is32kMode
-        .firstBank = 0,
+     .size = 0x8000 /* Patched to 0x4000 if !is32kMode */,
+     .firstBank = 0,
      .lastBank = 0,
      },
     {
-     // SECTTYPE_HRAM
-        .name = "HRAM"s,
+     .name = "HRAM"s,
      .startAddr = 0xFF80,
      .size = 0x007F,
      .firstBank = 0,
      .lastBank = 0,
      },
     {
-     // SECTTYPE_WRAMX
-        .name = "WRAMX"s,
+     .name = "WRAMX"s,
      .startAddr = 0xD000,
      .size = 0x1000,
      .firstBank = 1,
      .lastBank = 7,
      },
     {
-     // SECTTYPE_SRAM
-        .name = "SRAM"s,
+     .name = "SRAM"s,
      .startAddr = 0xA000,
      .size = 0x2000,
      .firstBank = 0,
      .lastBank = 255,
      },
     {
-     // SECTTYPE_OAM
-        .name = "OAM"s,
+     .name = "OAM"s,
      .startAddr = 0xFE00,
      .size = 0x00A0,
      .firstBank = 0,
