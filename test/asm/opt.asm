618c06be43ca4721fb93a0d464e1a2a6aff2cb6c
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-03-22 15:14:11 -0400

    Remove the deprecated `-H/-h/-L/-l` options

--- test/asm/opt.asm
+++ test/asm/opt.asm
@@ -1,22 +1,14 @@
 SECTION "test", ROM0
 
-	opt !h, !L ; already the default, but tests parsing "!"
-
 pusho
-	opt p42, Q.4, h, L, Wno-div
+	opt p42, Q.4, Wno-div
 	ds 1
-	ld [$ff88], a
-	halt
 	println $8000_0000 / -1
 	def n = 3.14
 	println "{x:n} = {f:n}"
 popo
 
-	opt H, l
-
 	ds 1
-	ld [$ff88], a
-	halt
 	println $8000_0000 / -1
 	def n = 3.14
 	println "{x:n} = {f:n}"
