5a2f1615fbe62cd6cad55a0fd944444f14060f5f
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-03-27 15:29:37 -0400

    Implement smart linking

--- include/link/patch.hpp
+++ include/link/patch.hpp
@@ -3,6 +3,12 @@
 #ifndef RGBDS_LINK_PATCH_HPP
 #define RGBDS_LINK_PATCH_HPP
 
+#include <vector>
+
+struct Patch;
+struct Section;
+struct Symbol;
+
 /*
  * Checks all assertions
  * @return true if assertion failed
@@ -13,5 +19,13 @@ void patch_CheckAssertions();
  * Applies all SECTIONs' patches to them
  */
 void patch_ApplyPatches();
+
+/**
+ * Executes a callback on all sections referenced by a patch's expression
+ * @param patch The patch to scan the expression of
+ */
+void patch_FindReferencedSections(
+    Patch const &patch, void (*callback)(Section &), std::vector<Symbol> const &fileSymbols
+);
 
 #endif // RGBDS_LINK_PATCH_HPP
