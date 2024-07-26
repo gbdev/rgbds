1f8f28cac8e36ad69e4370b3ae7b0a8dc79cf19c
ISSOtm <me@eldred.fr>
2024-07-26 19:36:26 +0200

    Fix parsing of textual colours
    `n`'s input value was not honored, and its updating logic incorrect

--- src/gfx/pal_spec.cpp
+++ src/gfx/pal_spec.cpp
@@ -240,11 +240,11 @@ static bool readLine(std::filebuf &file, std::string &buffer) {
 template<typename U> // Should be uint*_t
 static std::optional<U> parseDec(std::string const &str, std::string::size_type &n) {
 	uintmax_t value = 0;
-	auto result = std::from_chars(str.data(), str.data() + str.size(), value);
-	if ((bool)result.ec) {
+	auto result = std::from_chars(str.data() + n, str.data() + str.size(), value);
+	if (static_cast<bool>(result.ec)) {
 		return std::nullopt;
 	}
-	n += result.ptr - str.data();
+	n = result.ptr - str.data();
 	return std::optional<U>{value};
 }
 
