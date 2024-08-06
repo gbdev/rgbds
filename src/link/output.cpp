43bfc02975d5d8ff9cf78e5d2e53ccac6e7ed73a
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-08-06 10:43:49 -0400

    Sort constants by value then name, and apply review comment

--- src/link/output.cpp
+++ src/link/output.cpp
@@ -567,11 +567,16 @@ static void writeSym() {
 		if (std::holds_alternative<int32_t>(sym.data))
 			constants.push_back(&sym);
 	});
+	// Numeric constants are ordered by value, then by name
 	std::sort(RANGE(constants), [](Symbol *sym1, Symbol *sym2) -> bool {
-		return sym1->name < sym2->name;
+		int32_t val1 = std::get<int32_t>(sym1->data), val2 = std::get<int32_t>(sym2->data);
+		return val1 != val2 ? val1 < val2 : sym1->name < sym2->name;
 	});
-	for (Symbol *sym : constants)
-		fprintf(symFile, "%" PRIx32 " %s\n", std::get<int32_t>(sym->data), sym->name.c_str());
+	for (Symbol *sym : constants) {
+		int32_t val = std::get<int32_t>(sym->data);
+		int width = val < 0x100 ? 2 : val < 0x10000 ? 4 : 8;
+		fprintf(symFile, "%0*" PRIx32 " %s\n", width, val, sym->name.c_str());
+	}
 }
 
 // Writes the map file, if applicable.
