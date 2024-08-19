731715ff3671359862a7939ae96a5488794cd0e0
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-08-18 21:13:48 -0400

    Refactor test.sh scripts for consistency

--- test/gfx/test.sh
+++ test/gfx/test.sh
@@ -3,8 +3,15 @@
 [[ -e ./rgbgfx_test ]] || make -C ../.. test/gfx/rgbgfx_test Q= ${CXX:+"CXX=$CXX"} || exit
 [[ -e ./randtilegen ]] || make -C ../.. test/gfx/randtilegen Q= ${CXX:+"CXX=$CXX"} || exit
 
-trap 'rm -f "$errtmp"' EXIT
 errtmp="$(mktemp)"
+
+# Immediate expansion is the desired behavior.
+# shellcheck disable=SC2064
+trap "rm -f ${errtmp@Q}" EXIT
+
+tests=0
+failed=0
+rc=0
 
 bold="$(tput bold)"
 resbold="$(tput sgr0)"
@@ -14,19 +21,17 @@ rescolors="$(tput op)"
 
 RGBGFX=../../rgbgfx
 
-tests=0
-failed=0
-rc=0
-
-new_test() {
+newTest () {
 	cmdline="$*"
 	echo "${bold}${green}Testing: ${cmdline}${rescolors}${resbold}" >&2
 }
-test() {
+
+runTest () {
 	(( tests++ ))
 	eval "$cmdline"
 }
-fail() {
+
+failTest () {
 	rc=1
 	(( failed++ ))
 	echo "${bold}${red}Test ${cmdline} failed!${1:+ (RC=$1)}${rescolors}${resbold}"
@@ -39,8 +44,8 @@ while [[ "$ofs" -eq 0 ]]; do (( ofs = RANDOM % 256 )); done
 while [[ "$size" -eq 0 ]]; do (( size = RANDOM % 256 )); done
 for f in *.bin; do
 	for flags in ""{," -b $ofs"}{," -N $size,256"}; do
-		new_test ./rgbgfx_test "$f" $flags
-		test || fail $?
+		newTest ./rgbgfx_test "$f" $flags
+		runTest || failTest $?
 	done
 done
 
@@ -48,8 +53,8 @@ done
 reverse_cmd="$RGBGFX -c#none,#fff,#000 -o none_round_trip.2bpp -r 1 out.png"
 reconvert_cmd="$RGBGFX -c#none,#fff,#000 -o result.2bpp out.png"
 compare_cmd="cmp none_round_trip.2bpp result.2bpp"
-new_test "$reverse_cmd && $reconvert_cmd && $compare_cmd"
-test || fail $?
+newTest "$reverse_cmd && $reconvert_cmd && $compare_cmd"
+runTest || failTest $?
 
 # Remove temporaries (also ignored by Git) created by the above tests
 rm -f out*.png result.png result.2bpp
@@ -57,20 +62,20 @@ rm -f out*.png result.png result.2bpp
 for f in *.png; do
 	flags="$([[ -e "${f%.png}.flags" ]] && echo "@${f%.png}.flags")"
 
-	new_test "$RGBGFX" $flags "$f"
+	newTest "$RGBGFX" $flags "$f"
 	if [[ -e "${f%.png}.err" ]]; then
-		test 2>"$errtmp"
-		diff -u --strip-trailing-cr "${f%.png}.err" "$errtmp" || fail
+		runTest 2>"$errtmp"
+		diff -u --strip-trailing-cr "${f%.png}.err" "$errtmp" || failTest
 	else
-		test || fail $?
+		runTest || failTest $?
 	fi
 
-	new_test "$RGBGFX" $flags - "<$f"
+	newTest "$RGBGFX" $flags - "<$f"
 	if [[ -e "${f%.png}.err" ]]; then
-		test 2>"$errtmp"
-		diff -u --strip-trailing-cr <(sed "s/$f/<stdin>/g" "${f%.png}.err") "$errtmp" || fail
+		runTest 2>"$errtmp"
+		diff -u --strip-trailing-cr <(sed "s/$f/<stdin>/g" "${f%.png}.err") "$errtmp" || failTest
 	else
-		test || fail $?
+		runTest || failTest $?
 	fi
 done
 
