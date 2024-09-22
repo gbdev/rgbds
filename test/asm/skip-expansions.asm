15919e550ffe4461e3c7d908897db324d48500a6
Sylvie <35663410+Rangi42@users.noreply.github.com>
2024-09-22 15:31:12 -0400

    Add test to demonstrate lack of expansions in `skipIfBlock` (#1516)

---
+++ test/asm/skip-expansions.asm
@@ -0,0 +1,31 @@
+; skipping ahead to `elif`/`else`/`endc`/`endr`/`endm` disables expansions!
+
+MACRO mac1
+	if _NARG != 2
+		println "args: \#"
+	elif (\2) == 42  
+		println "forty-two!"
+	else
+		println "it's ", (\2)
+	endc
+ENDM
+
+	mac1 elif, 6 * 7  ; this prints "forty-two!" because it takes the `elif`
+	mac1 elif, 6 * 9
+	mac1 elif
+	mac1
+
+MACRO mac2
+	if _NARG != 2
+		println "args: \#"
+	\1 (\2) == 42  ; if the `if` is not taken, this is not expanded!
+		println "forty-two!"
+	else
+		println "it's ", (\2)
+	endc
+ENDM
+
+	mac2 elif, 6 * 7  ; this prints "it's $2A" because it skips the `\1` line and takes the `else`
+	mac2 elif, 6 * 9
+	mac2 elif
+	mac2
