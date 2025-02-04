fbde24ee1795df4fac9206a0792da6b061b2051c
Rangi <35663410+Rangi42@users.noreply.github.com>
2025-02-04 10:40:38 +0100

    Add contrib/checkformat.bash to check for clang-formatting (#1646)

---
+++ contrib/checkformat.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: MIT
+
+clang-format --version
+
+find . -type f \( -iname '*.hpp' -o -iname '*.cpp' \) -exec clang-format -i {} +
+
+if ! git diff-index --quiet HEAD --; then
+	echo 'Unformatted files:'
+	git diff-index --name-only HEAD --
+	echo
+	git diff HEAD --
+	return 1
+fi
