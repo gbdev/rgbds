e0a8eb8affa767c9a3b4d70c08fdcf4fd209d003
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-04-24 09:52:08 -0400

    Update test dependencies

--- test/fetch-test-deps.sh
+++ test/fetch-test-deps.sh
@@ -107,10 +107,10 @@ if ! "$external"; then
 fi
 
 if "$nonfree"; then
-	action pret  pokecrystal      2024-12-28 86a87a355e6805663cfecf150a3af71c66441a3e
-	action pret  pokered          2025-01-29 43f21cc4948682b51b4c72bcef38631f95b47bfe
-	action zladx LADX-Disassembly 2025-01-06 e5ab607f39b0a4bd35f0bae6c6354fde9eecca3c
+	action pret  pokecrystal      2025-04-20 098944485b76d6bd758ead442f3f17ac29aacd39
+	action pret  pokered          2025-04-19 d47f74ee1fed608f902dbc0fd51634dd7bafc6e4
+	action zladx LADX-Disassembly 2025-04-12 8db9dbbd1dcbbdb4df7c455f56d0ad1fa52b124b
 fi
-action AntonioND ucity   2024-12-26 2056166b0a84271d6c2a5a1f7d037dd416085796
-action pinobatch libbet  2024-12-26 de2081260d6111a3e60b16120bbb905ef00c6a34
-action LIJI32    SameBoy 2025-01-23 1798edf4c1f8cbf018d14436f14c2a18ff256ae4
+action AntonioND ucity   2025-03-20 04cdda3dda35e62501cf9543b73343935039ee4d
+action pinobatch libbet  2025-03-28 c564dc01baa91bd6123ec64d877630dd392e74b5
+action LIJI32    SameBoy 2025-04-07 1cf84a5436c49413ae756c9e9c18ce71e96d7990
