d0b78c061d8d71445134da33f6c0e76a2d701278
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-07-12 10:02:27 -0400

    Dark mode

--- contrib/coverage.bash
+++ contrib/coverage.bash
@@ -14,11 +14,11 @@ popd
 gcov src/**/*.cpp
 mkdir -p coverage
 
-# Generate coverage report
+# Generate coverage report, excluding Bison-generated files
 COVERAGE_INFO=coverage/coverage.info
-lcov -c --no-external -d . -o $COVERAGE_INFO
-lcov -v -r $COVERAGE_INFO src/asm/parser.{hpp,cpp} src/link/script.{hpp,cpp} -o $COVERAGE_INFO
-genhtml -f -s -o coverage/ $COVERAGE_INFO
+lcov -c --no-external -d . -o "$COVERAGE_INFO"
+lcov -r "$COVERAGE_INFO" src/asm/parser.{hpp,cpp} src/link/script.{hpp,cpp} -o "$COVERAGE_INFO"
+genhtml --dark-mode -f -s -o coverage/ "$COVERAGE_INFO"
 
 # Check whether running from coverage.yml workflow
 if [ "$1" != "false" ]; then
