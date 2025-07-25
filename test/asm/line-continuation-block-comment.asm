87c10988ed677f2e37605ad0d54f2e1410ae6f52
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-07-25 07:47:19 -0400

    Add test case for block comments after line continuations

---
+++ test/asm/line-continuation-block-comment.asm
@@ -0,0 +1,18 @@
+println 2 / 2
+
+println 6 / \ ; comment
+3
+
+println 6 \ / ; comment
+2
+
+println 8 / /* hello
+world */ 2
+
+println 10 \ / 2
+
+println 12 \ / /* hello
+world */ 2
+
+println 14 / \ /* hello
+world */ 2
