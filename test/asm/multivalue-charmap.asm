f24d7dd85582ee5c27065161d58829b26347a269
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-08-05 16:00:02 -0400

    Fix broken tests

--- test/asm/multivalue-charmap.asm
+++ test/asm/multivalue-charmap.asm
@@ -24,12 +24,12 @@ dl "AB" ; dl $01234567, $fedcba98
 
 charmap "C", $01, $23, $45, $67
 charmap "D", $fe, $dc, $ba, $98
-assert "C" == $01234567
-assert "D" == $fedcba98
+assert CHARVAL("C", 0) == $01 && CHARVAL("C", 3) == $67
+assert CHARVAL("D", 1) == $dc && CHARVAL("D", 2) == $ba
 db "CD" ; db $01, $23, $45, $67, $fe, $dc, $ba, $98
 dw "CD" ; dw $01, $23, $45, $67, $fe, $dc, $ba, $98
 
 charmap "E", $01, $2345, $6789ab, $cdef
-assert "E" == $0145abef
+assert CHARSIZE("E") == 4 && CHARVAL("E", 2) == $6789ab
 db "E" ; db $01, $2345, $6789ab, $cdef (truncated to $01, $45, $ab, $ef)
 dl "E" ; dl $01, $2345, $6789ab, $cdef
