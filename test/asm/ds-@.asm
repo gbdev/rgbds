1574b5b1f76117e4d761172c9175958a01227402
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-08-07 09:41:33 -0400

    Test the behavior of `ds N, @`

--- test/asm/ds-@.asm
+++ test/asm/ds-@.asm
@@ -1,14 +1,16 @@
 SECTION "test fixed", ROM0[0]
 
 FixedStart:
-	ds 6, (@ - FixedStart) * 2 + zero
-	ds 10, (@ - FixedStart) + zero, (@ - FixedStart) * 3 + zero, (@ - FixedStart) * 4 + zero
+	ds 4, @
+	ds 4, (@ - FixedStart) * 2 + zero
+	ds 8, (@ - FixedStart) + zero, (@ - FixedStart) * 3 + zero, (@ - FixedStart) * 4 + zero
 
 SECTION "test floating", ROM0
 
 FloatingStart:
-	ds 6, (@ - FloatingStart) * 2 + zero
-	ds 10, (@ - FloatingStart) + zero, (@ - FloatingStart) * 3 + zero, (@ - FloatingStart) * 4 + zero
+	ds 4, @
+	ds 4, (@ - FloatingStart) * 2 + zero
+	ds 8, (@ - FloatingStart) + zero, (@ - FloatingStart) * 3 + zero, (@ - FloatingStart) * 4 + zero
 
 SECTION "zero", ROM0[0]
 zero:
