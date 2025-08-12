5431182c412734c4c1ab569a238065f84efe50be
Rangi42 <sylvie.oukaour+rangi42@gmail.com>
2025-08-11 20:19:49 -0400

    Add tests

--- test/link/patch-diagnostics.asm
+++ test/link/patch-diagnostics.asm
@@ -1,6 +1,7 @@
 def fzero equs "startof(\"test\")"
 section "test", rom0
 ld a, $8000_0000 / (fzero - 1)
+ld a, $8000_0000 / (fzero - 2)
 ld a, 1 << (fzero - 1)
 ld a, 1 << (fzero + 32)
 ld a, (fzero - 1) >> 1
