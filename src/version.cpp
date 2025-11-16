0f266d1c665914b638df029be24ead488113e07e
Rangi <35663410+Rangi42@users.noreply.github.com>
2025-11-16 17:11:09 -0500

    Specify more ASan options (#1860)

--- src/version.cpp
+++ src/version.cpp
@@ -14,7 +14,13 @@
 #if !defined(NDEBUG) && defined(__SANITIZE_ADDRESS__) && !defined(__APPLE__)
 extern "C" {
 	char const *__asan_default_options(void) {
-		return "detect_leaks=1";
+		return "detect_leaks=1"
+		       ":detect_stack_use_after_return=1"
+		       ":detect_invalid_pointer_pairs=2"
+		       ":check_initialization_order=1"
+		       ":strict_init_order=1"
+		       ":strict_string_checks=1"
+		       ":print_legend=0";
 	}
 }
 #endif
