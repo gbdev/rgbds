80335bbe3afd79e4b1a02bb41a33acd295163e48
Rangi <sylvie.oukaour+rangi42@gmail.com>
2025-12-05 15:23:42 -0500

    Refactor `FileStackNode::printBacktrace` from recursive to iterative
    This avoids a potential stack overflow for very long backtraces,
    or for corrupt object files with cyclic backtraces

--- src/link/fstack.cpp
+++ src/link/fstack.cpp
@@ -12,44 +12,47 @@
 
 #include "link/warning.hpp"
 
-using TraceNode = std::pair<std::string, uint32_t>;
-
-static std::vector<TraceNode> backtrace(FileStackNode const &node, uint32_t curLineNo) {
-	if (node.isQuiet && !tracing.loud) {
-		if (node.parent) {
+void FileStackNode::printBacktrace(uint32_t curLineNo) const {
+	using TraceItem = std::pair<FileStackNode const *, uint32_t>;
+	std::vector<TraceItem> items;
+	for (TraceItem item{this, curLineNo};;) {
+		auto &[node, itemLineNo] = item;
+		bool loud = !node->isQuiet || tracing.loud;
+		if (loud) {
+			items.emplace_back(node, itemLineNo);
+		}
+		if (!node->parent) {
+			assume(node->type != NODE_REPT && std::holds_alternative<std::string>(node->data));
+			break;
+		}
+		if (loud || node->type != NODE_REPT) {
 			// Quiet REPT nodes will pass their interior line number up to their parent,
 			// which is more precise than the parent's own line number (since that will be
 			// the line number of the "REPT?" or "FOR?" itself).
-			return backtrace(*node.parent, node.type == NODE_REPT ? curLineNo : node.lineNo);
+			itemLineNo = node->lineNo;
 		}
-		return {}; // LCOV_EXCL_LINE
+		node = &*node->parent;
 	}
 
-	if (!node.parent) {
-		assume(node.type != NODE_REPT && std::holds_alternative<std::string>(node.data));
-		return {
-		    {node.name(), curLineNo}
-		};
-	}
-
-	std::vector<TraceNode> traceNodes = backtrace(*node.parent, node.lineNo);
-	if (std::holds_alternative<std::vector<uint32_t>>(node.data)) {
-		assume(!traceNodes.empty()); // REPT nodes use their parent's name
-		std::string reptName = traceNodes.back().first;
-		if (std::vector<uint32_t> const &nodeIters = node.iters(); !nodeIters.empty()) {
-			reptName.append(NODE_SEPARATOR REPT_NODE_PREFIX);
-			reptName.append(std::to_string(nodeIters.back()));
+	using TraceNode = std::pair<std::string, uint32_t>;
+	std::vector<TraceNode> traceNodes;
+	traceNodes.reserve(items.size());
+	for (auto &[node, itemLineNo] : reversed(items)) {
+		if (std::holds_alternative<std::vector<uint32_t>>(node->data)) {
+			assume(!traceNodes.empty()); // REPT nodes use their parent's name
+			std::string reptName = traceNodes.back().first;
+			if (std::vector<uint32_t> const &nodeIters = node->iters(); !nodeIters.empty()) {
+				reptName.append(NODE_SEPARATOR REPT_NODE_PREFIX);
+				reptName.append(std::to_string(nodeIters.back()));
+			}
+			traceNodes.emplace_back(reptName, itemLineNo);
+		} else {
+			traceNodes.emplace_back(node->name(), itemLineNo);
 		}
-		traceNodes.emplace_back(reptName, curLineNo);
-	} else {
-		traceNodes.emplace_back(node.name(), curLineNo);
 	}
-	return traceNodes;
-}
 
-void FileStackNode::printBacktrace(uint32_t curLineNo) const {
 	trace_PrintBacktrace(
-	    backtrace(*this, curLineNo),
+	    traceNodes,
 	    [](TraceNode const &node) { return node.first.c_str(); },
 	    [](TraceNode const &node) { return node.second; }
 	);
