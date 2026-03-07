65b0b49720e20343d633551f68321892e3483878
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-03-06 22:01:14 -0500

    Update zlib to 1.3.2
    Note that the DLL name created by CMake with `BUILD_SHARED_LIBS`
    has changed from `zlib1.dll` to `z.dll`. This was caused by the
    "Continued rewrite of CMake build [Vollstrecker]" from its
    changelog <https://zlib.net/ChangeLog.txt>, and has been reported
    in <https://github.com/madler/zlib/issues/1181>.
    
    The MinGW package `libz-mingw-w64-dev` still calls it `libz1.dll`.

--- .github/scripts/get_win_deps.ps1
+++ .github/scripts/get_win_deps.ps1
@@ -15,9 +15,9 @@ function getlibrary ([string] $URI, [string] $filename, [string] $hash, [string]
 	Expand-Archive -DestinationPath $destdir $filename
 }
 
-getlibrary 'https://www.zlib.net/zlib131.zip' 'zlib.zip' '72af66d44fcc14c22013b46b814d5d2514673dda3d115e64b690c1ad636e7b17' .
+getlibrary 'https://www.zlib.net/zlib132.zip' 'zlib.zip' 'e8bf55f3017aa181690990cb58a994e77885da140609fc8f94abe9b65d2cae28' .
 getlibrary 'https://github.com/pnggroup/libpng/archive/refs/tags/v1.6.53.zip' 'libpng.zip' '9fb99118ec4523d9a9dab652ce7c2472ec76f6ccd69d1aba3ab873bb8cf84b98' .
 getlibrary 'https://github.com/lexxmark/winflexbison/releases/download/v2.5.25/win_flex_bison-2.5.25.zip' 'winflexbison.zip' '8d324b62be33604b2c45ad1dd34ab93d722534448f55a16ca7292de32b6ac135' install_dir
 
-Move-Item zlib-1.3.1 zlib
+Move-Item zlib-1.3.2 zlib
 Move-Item libpng-1.6.53 libpng
