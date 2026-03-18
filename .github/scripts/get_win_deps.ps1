d6d1844d60559505fdf7735970a674d8da9d6002
ISSOtm <me@eldred.fr>
2026-03-18 02:34:30 +0100

    Seek lib deps via CMake FetchContent
    `FetchContent` respects existing installs, and downloads and compiles the libs
    if they aren't found.
    This is admittedly a little finicky, since this ignores the usual `Find*`
    modules provided by CMake, requiring a bit of glue on our side.
    
    But, one upside is that this moves that logic from our CI into the build system,
    which can thus benefit other downstream users.
    This also opens the door to some improvements in upcoming commits.
    
    Doing this with Bison turned out to be much more painful, however, due to
    `FindBISON` providing the specific `bison_target` command; thus, it remains
    installed externally, so that it can be picked up by `FindBISON`.
    
    This also bumps our CMake version requirement slightly, though it's
    possible that older versions keep working, or could be supported with small patches;
    however, our CI doesn't provide anything below 3.31, so we can't check.

--- .github/scripts/get_win_deps.ps1
+++ .github/scripts/get_win_deps.ps1
@@ -15,9 +15,4 @@ function getlibrary ([string] $URI, [string] $filename, [string] $hash, [string]
 	Expand-Archive -DestinationPath $destdir $filename
 }
 
-getlibrary 'https://www.zlib.net/zlib132.zip' 'zlib.zip' 'e8bf55f3017aa181690990cb58a994e77885da140609fc8f94abe9b65d2cae28' .
-getlibrary 'https://github.com/pnggroup/libpng/archive/refs/tags/v1.6.53.zip' 'libpng.zip' '9fb99118ec4523d9a9dab652ce7c2472ec76f6ccd69d1aba3ab873bb8cf84b98' .
-getlibrary 'https://github.com/lexxmark/winflexbison/releases/download/v2.5.25/win_flex_bison-2.5.25.zip' 'winflexbison.zip' '8d324b62be33604b2c45ad1dd34ab93d722534448f55a16ca7292de32b6ac135' install_dir
-
-Move-Item zlib-1.3.2 zlib
-Move-Item libpng-1.6.53 libpng
+getlibrary 'https://github.com/lexxmark/winflexbison/releases/download/v2.5.25/win_flex_bison-2.5.25.zip' 'winflexbison.zip' '8d324b62be33604b2c45ad1dd34ab93d722534448f55a16ca7292de32b6ac135' bison
