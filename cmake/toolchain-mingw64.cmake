4bdddbddd08f445690f5080233670e4195ea73eb
ISSOtm <me@eldred.fr>
2026-03-19 21:44:18 +0100

    Switch MinGW building to CMake
    Automatically grabs and compiles libpng, and avoids having hacky lines in our Makefile
    (the compiler specification *should* be orthogonal to the build target!)
    
    Also move the MinGW package install to the `install_deps.sh` script,
    to move logic off of the YAML.

---
+++ cmake/toolchain-mingw64.cmake
@@ -0,0 +1,22 @@
+# From https://www.mingw-w64.org/build-systems/cmake/
+
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# specify the cross compiler
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+# where is the target environment
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+
+# search for programs in the build host directories
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# CMake determines how to examine dependencies based on the *host* system, leading to
+# a `file unknown error` unless the target platform is explicitly specified.
+set(CMAKE_GET_RUNTIME_DEPENDENCIES_PLATFORM "windows+pe")
