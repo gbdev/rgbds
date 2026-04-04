375a7c73b3736470b8be01a17345f67c2f329678
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-04-04 13:20:14 -0400

    Fix some fixed-point literal lexing issues
    - No fractional digits are necessary after the decimal point,
      e.g. `42.` is valid instead of `42.0`
    - Error messages refer to "fixed-point" not "integer" constants
    - Test more carefully for lexing unrelated to underscores

---
+++ test/asm/fixed-point-syntax.asm
@@ -0,0 +1,15 @@
+; good
+println 3.14
+println 42.
+println 12_34.56_78
+println 12.34_q.5
+println 1_2.3_4_q15
+println 1.q2
+
+; bad
+println 12.34q0
+println 12.34q_15
+println 12.34q1_5
+println 1_.2
+println 1._2
+println 1.2q
