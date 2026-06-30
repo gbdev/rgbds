d59081a91820ddd3c84b9084a2c46e6c85d48b27
ISSOtm <me@eldred.fr>
2026-06-30 17:12:22 +0200

    Shorten external test prop names slightly
    Makes the scripts a bit easier to read, I think

--- test/external/test.sh
+++ test/external/test.sh
@@ -15,20 +15,20 @@ if [ ! -f "external/$1.cfg" ]; then
 	exit 1
 fi
 
-# Sourcing "external/$1.cfg" defines `EXTERNAL_TEST_*` values used below.
+# Sourcing "external/$1.cfg" defines `EXT_TEST_*` values used below.
 . "external/$1.cfg"
 
-if ! cd "$EXTERNAL_TEST_REPO"; then
+if ! cd "$EXT_TEST_REPO"; then
 	echo >&2 'Please fetch test deps before running any external test'
 	exit 1
 fi
 make clean $RGBDS_PATH
-make -j4 "$EXTERNAL_TEST_TARGET" $RGBDS_PATH
-hash="$(sha1sum -b "$EXTERNAL_TEST_FILE" | head -c 40)"
-if [ "$hash" != "$EXTERNAL_TEST_HASH" ]; then
+make -j4 "$EXT_TEST_TARGET" $RGBDS_PATH
+hash="$(sha1sum -b "$EXT_TEST_FILE" | head -c 40)"
+if [ "$hash" != "$EXT_TEST_HASH" ]; then
 	cat >&2 <<EOM
-error: "$EXTERNAL_TEST_FILE" checksum did not match!
-    Expected $EXTERNAL_TEST_HASH,
+error: "$EXT_TEST_FILE" checksum did not match!
+    Expected $EXT_TEST_HASH,
          got $hash
 EOM
 	exit 1
