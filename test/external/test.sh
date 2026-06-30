eb949f51a6ec24287acc53a40bd1961ff91df597
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-06-29 21:10:38 -0400

    No need for a single-use `test_downstream` function

--- test/external/test.sh
+++ test/external/test.sh
@@ -10,25 +10,23 @@ cd "$(dirname "$0")/.."
 
 RGBDS_PATH="RGBDS=../../"
 
-test_downstream() {
-	if ! pushd "$EXTERNAL_TEST_REPO"; then
-		echo >&2 'Please fetch test deps before running any external test'
-		return 1
-	fi
-	make clean $RGBDS_PATH
-	make -j4 "$EXTERNAL_TEST_TARGET" $RGBDS_PATH
-	hash="$(sha1sum -b "$EXTERNAL_TEST_FILE" | head -c 40)"
-	if [ "$hash" != "$EXTERNAL_TEST_HASH" ]; then
-		echo >&2 'SHA-1 hash of '"$EXTERNAL_TEST_FILE"' did not match: '"$hash"
-		return 1
-	fi
-	popd
-}
-
 if [ ! -f "external/$1.cfg" ]; then
 	echo >&2 'External test file '"$1"'.cfg does not exist'
 	exit 1
 fi
 
-# Sourcing "external/$1.cfg" defines `EXTERNAL_TEST_*` values used by `test_downstream`.
-. "external/$1.cfg" && test_downstream
+# Sourcing "external/$1.cfg" defines `EXTERNAL_TEST_*` values used below.
+. "external/$1.cfg"
+
+if ! pushd "$EXTERNAL_TEST_REPO"; then
+	echo >&2 'Please fetch test deps before running any external test'
+	exit 1
+fi
+make clean $RGBDS_PATH
+make -j4 "$EXTERNAL_TEST_TARGET" $RGBDS_PATH
+hash="$(sha1sum -b "$EXTERNAL_TEST_FILE" | head -c 40)"
+if [ "$hash" != "$EXTERNAL_TEST_HASH" ]; then
+	echo >&2 'SHA-1 hash of '"$EXTERNAL_TEST_FILE"' did not match: '"$hash"
+	exit 1
+fi
+popd
