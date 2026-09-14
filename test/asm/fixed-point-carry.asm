63456573c0972c75a34133c3d18fcb1149a4a2c8
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-09-14 13:09:30 -0400

    Fix rounding of fixed-point constants where fractional digits should carry into integer part

---
+++ test/asm/fixed-point-carry.asm
@@ -0,0 +1,17 @@
+opt Q8
+println strfmt("%f", 1.999)
+println strfmt("%f", 2.999)
+println strfmt("%f", 3.999)
+println strfmt("%f", 16777215.999)
+
+opt Q16
+println strfmt("%f", 1.999999)
+println strfmt("%f", 2.999999)
+println strfmt("%f", 3.999999)
+println strfmt("%f", 65535.999999)
+
+opt Q24
+println strfmt("%f", 1.999999999)
+println strfmt("%f", 2.999999999)
+println strfmt("%f", 3.999999999)
+println strfmt("%f", 255.999999999)
