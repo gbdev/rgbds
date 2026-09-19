92579f5c0dbebd8181a327db9beb3621189b5d29
Rangi <sylvie.oukaour+rangi42@gmail.com>
2026-09-19 11:46:05 -0400

    Define more debug options for `make develop`

--- src/version.cpp
+++ src/version.cpp
@@ -14,13 +14,22 @@
 #if !defined(NDEBUG) && defined(__SANITIZE_ADDRESS__) && !defined(__APPLE__)
 extern "C" {
 	char const *__asan_default_options(void) {
-		return "detect_leaks=1"
-		       ":detect_stack_use_after_return=1"
+		return ":check_initialization_order=1"
 		       ":detect_invalid_pointer_pairs=2"
-		       ":check_initialization_order=1"
+		       ":detect_leaks=1"
+		       ":detect_stack_use_after_return=1"
+		       // ":fast_unwind_on_malloc=0" // Enable this if ASan outputs bad backtraces
+		       ":print_legend=0"
 		       ":strict_init_order=1"
-		       ":strict_string_checks=1"
-		       ":print_legend=0";
+		       ":strict_string_checks=1";
+	}
+}
+#endif
+
+#if !defined(NDEBUG) && defined(__SANITIZE_UNDEFINED__)
+extern "C" {
+	char const *__ubsan_default_options(void) {
+		return "print_stacktrace=1";
 	}
 }
 #endif
